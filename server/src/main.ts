505e0bf6fd8b960e378fdc881e6e96fe9f68ef63
Louise Lizé <louise.lize@icloud.com>
2024-06-26 21:04:47 +0200

    ✨ Add Module, Controller and DTO for polls

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,20 +5,12 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const clientPort = parseInt(configService.get('CLIENT_PORT'));
   const port = parseInt(configService.get('PORT'));
-  
-  await app.listen(clientPort);
+
   logger.log(`Client running on port ${clientPort}`);
   app.enableCors({
     origin: [
