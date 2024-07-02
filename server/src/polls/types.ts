610fde0f489c22969f0a1405faa318d693bffecd
Louise Lizé <louise.lize@icloud.com>
2024-07-02 19:58:03 +0200

    ✨ Add NestJS Repository to work with RedisJSON

--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -1,3 +1,4 @@
+// service types
 export type CreatePollFields = {
     topic: string;
     votesPerVoter: number;
@@ -14,3 +15,17 @@ export type CreatePollFields = {
     userID: string;
     name: string;
   };
+
+// repository types
+export type CreatePollData = {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+};
+
+export type AddParticipantData = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
